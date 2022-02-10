0e3c6c1e6bf7e512d440355a139ef083812758e8
Alysson Rodrigues <rodrigues.alysson@gmail.com>
2022-02-10 08:55:23 -0300

    cração dos métodos das interfaces de cliente e conta

--- banco-digital/src/main/java/me/dio/banco/ports/ClienteRepository.java
+++ banco-digital/src/main/java/me/dio/banco/ports/ClienteRepository.java
@@ -1,5 +1,12 @@
 package me.dio.banco.ports;
 
+import me.dio.banco.dominio.Banco;
+import me.dio.banco.dominio.Cliente;
+
 public interface ClienteRepository {
+
+	Cliente cadastrarCliente(Banco banco, String cpf);
+
+	Cliente buscaClientePeloCPF(Banco banco, String cpf);
 
 }
--- banco-digital/src/main/java/me/dio/banco/ports/ContaCorrenteRespository.java
+++ banco-digital/src/main/java/me/dio/banco/ports/ContaCorrenteRespository.java
@@ -1,5 +1,16 @@
 package me.dio.banco.ports;
 
+import me.dio.banco.dominio.Banco;
+import me.dio.banco.dominio.Cliente;
+import me.dio.banco.dominio.Conta;
+import me.dio.banco.dominio.ContaCorrente;
+
 public interface ContaCorrenteRespository {
+
+	void cadastrarConta(Banco banco, Cliente cliente);
+
+	ContaCorrente cadastrarConta(Banco banco, String cpf);
+
+	void extrato(Conta conta);
 
 }
